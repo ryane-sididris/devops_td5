96518d585ee4398fc8f6de289e39d1009c05e945
Ryane Sid Idris <ryane.sididris@edu.esiee.fr>
2026-02-18 21:30:32 +0100

    Fix: correct backend bucket name to ryan-bucket-state-td5-fin

--- scripts/tofu/live/lambda-sample/backend.tf
+++ scripts/tofu/live/lambda-sample/backend.tf
@@ -1,11 +1,8 @@
 terraform {
   backend "s3" {
-    # TODO: fill in your own bucket name here!
-    bucket         = "ryan-bucket-state-td5-fin" 
-    key            = "ch5/tofu/live/lambda-sample"       
+    bucket         = "ryan-bucket-state-td5-fin"
+    key            = "devops-td5/lambda-sample/terraform.tfstate"
     region         = "us-east-2"
     encrypt        = true
-    # TODO: fill in your own DynamoDB table name here!
-    dynamodb_table = "fundamentals-of-devops-tofu-state" 
   }
 }
