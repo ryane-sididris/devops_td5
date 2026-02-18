f2b29c81c8b206bb9f4ddc133c635e9d6fe5039c
Ryane Sid Idris <ryane.sididris@edu.esiee.fr>
2026-02-18 21:33:47 +0100

    Fix

--- scripts/tofu/live/lambda-sample/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "ryan-bucket-state-td5-fin"
-    key            = "devops-td5/lambda-sample/terraform.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
-  }
-}
