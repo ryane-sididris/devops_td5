37567ee9aa151c1911f2ef66fc54d39eab6f9b11
Ryane Sid Idris <ryane.sididris@edu.esiee.fr>
2026-02-18 21:11:00 +0100

    syntax

--- scripts/tofu/modules/api-gateway/main.tf
+++ scripts/tofu/modules/api-gateway/main.tf
@@ -13,6 +13,23 @@ resource "aws_apigatewayv2_stage" "stage" {
   auto_deploy = true
 }
 
+# --- AJOUT DES RESSOURCES MANQUANTES ---
+
+resource "aws_apigatewayv2_integration" "lambda" {
+  api_id           = aws_apigatewayv2_api.api.id
+  integration_type = "AWS_PROXY"
+  integration_uri  = var.function_arn
+}
+
+resource "aws_apigatewayv2_route" "routes" {
+  count    = length(var.api_gateway_routes)
+  api_id    = aws_apigatewayv2_api.api.id
+  route_key = var.api_gateway_routes[count.index]
+  target    = "integrations/${aws_apigatewayv2_integration.lambda.id}"
+}
+
+# ---------------------------------------
+
 resource "aws_lambda_permission" "apigw" {
   action        = "lambda:InvokeFunction"
   function_name = var.function_arn
@@ -20,4 +37,6 @@ resource "aws_lambda_permission" "apigw" {
   source_arn    = "${aws_apigatewayv2_api.api.execution_arn}/*/*"
 }
 
-output "api_endpoint" { value = aws_apigatewayv2_api.api.api_endpoint }
+output "api_endpoint" { 
+  value = aws_apigatewayv2_api.api.api_endpoint 
+}
