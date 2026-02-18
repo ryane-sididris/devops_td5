64a6e05727e9ffc566153fb5a1fc14432ff98a8d
Ryane Sid Idris <ryane.sididris@edu.esiee.fr>
2026-02-18 20:46:33 +0100

    Fix: use local modules instead of broken github links

--- scripts/tofu/live/lambda-sample/main.tf
+++ scripts/tofu/live/lambda-sample/main.tf
@@ -3,7 +3,8 @@ provider "aws" {
 }
 
 module "function" {
-  source = "github.com/brikis98/devops-book//ch3/tofu/modules/lambda"
+  # On pointe vers ton dossier local au lieu de GitHub
+  source = "../../modules/lambda-sample" 
 
   name = var.name
 
@@ -20,7 +21,8 @@ module "function" {
 }
 
 module "gateway" {
-  source = "github.com/brikis98/devops-book//ch3/tofu/modules/api-gateway"
+  # On pointe vers ton dossier local au lieu de GitHub
+  source = "../../modules/api-gateway"
 
   name = var.name
   function_arn       = module.function.function_arn
